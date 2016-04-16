a089211ca76f7f64122ddb94d0761196180a8137
Matt Morrison <matt@mattmorrison.net>
2016-04-15 23:10:16 -0500

    Verify-Up
    This program tests whether a site is up or not by downloading a known
    file from the remote server. If the file is able to be downloaded, the
    site is considered to be up, and the URL to the remote site is served.
    If the file,  however, is not found, you are able to serve a mirror to
    the site. This example uses two instances of sites to be tested, but
    you can expand them to as many as you need.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset=utf-8>
+</head>
+
+<body>
+  <p><a href='verify-up.php?TestURL1=true'>TestURL1</a></p>
+  <p><a href='verify-up.php?TestURL2=true'>TestURL2</a></p>
+</body>
+
+</html>
---
+++ verify-up.php
@@ -0,0 +1,62 @@
+/**
+  File: verify-up.php
+
+  Description:
+
+  This program tests whether a site is up or not by downloading a known file
+  from the remote server. If the file is able to be downloaded, the site is
+  considered to be up, and the URL to the remote site is served. If the file,
+  however, is not found, you are able to serve a mirror to the site. This
+  example uses two instances of sites to be tested, but you can expand them to
+  as many as you need.
+  
+**/
+
+<?php
+// TestURL1 and TestURL2 can be replaced with the URL of a file on the remote site
+// you wish to test. Be sure to link a file that you know will not go away, such
+// as the index.html file at the root of the remote web server.
+if (isset($_GET['TestURL1'])) {
+  verify("TestURL1");
+} else if (isset($_GET['TestURL2'])) {
+  verify("TestURL2");
+}
+
+// $testsite is the argument which contains the link to the file you wish to see
+// if it exists on the remote server.
+function verify($testsite) {
+
+  $ch = curl_init($testsite);
+
+  curl_setopt($ch, CURLOPT_NOBODY, true);
+  curl_exec($ch);
+  $retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+  // $retcode >= 400 -> not found, $retcode = 200, found.
+  // Replace the TestURL with the address you set above. If the site exists, you
+  // can link to the TestURL
+  if ($retcode == 200 && $testsite == "TestURL1") {
+    echo '<script type="text/javascript">
+           window.location = "TestURL1"
+          </script>';
+  }
+  else if ($retcode == 200 && $testsite == "TestURL2") {
+    echo '<script type="text/javascript">
+           window.location = "TestURL2"
+          </script>';
+  }
+  // If $retcode !=200, the file on the site is not found. If this is the case,
+  // you can link to a mirror of the site so that your link never goes down.
+  else if ($retcode != 200 && $testsite == "TestURL1") {
+    echo '<script type="text/javascript">
+           window.location = "TestURL1-Mirror"
+          </script>';
+  }
+  else if ($retcode != 200 && $testsite == "TestURL2") {
+    echo '<script type="text/javascript">
+           window.location = "TestURL2-Mirror"
+          </script>';
+  }
+  curl_close($ch);
+}
+?>
